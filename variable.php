ed595ada433bd66aaeb3b88e630c3a9cbab0da8e
pateltanvi <84912377+pateltanvi@users.noreply.github.com>
2021-06-01 22:05:18 +0530

    DAY_2

---
+++ variable.php
@@ -0,0 +1,23 @@
+<?php
+  //variable
+  
+  $a = 2;
+  $b = 10;
+  
+  //method-1
+  echo " A value is " .$a;
+  echo " <br/> B value is " .$b;
+  
+  //Method-2
+   //echo "<br/>A value is".$a. "<br/>B value is" .$b.;
+   
+   //Method-3
+   echo "<br/> A value is $a <br/> B value is $b";
+   
+   //Method -4
+   echo ' <br/> A value is $a <br/> B value is $b';
+   
+   //Method-5
+  echo " <br/>A value is $a<br/>B value is $b sum is".($a+$b);
+  
+  
