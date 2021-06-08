1346b383605685578f0a4627ed2965584f472e71
pateltanvi <84912377+pateltanvi@users.noreply.github.com>
2021-06-08 23:02:03 +0530

    Theme

---
+++ DAY_4/master.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Smart Academy</title>
+
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body><div id="menu-wrapper">
+	<div id="menu">
+		<ul>
+			<li class="current_page_item"><a href="#">Homepage</a></li>
+			<li><a href="#">Photos</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</div>
+	<!-- end #menu --> 
+</div>
+<?php
+
+//include './Theme part/menu.php';
+include './Theme part/logo.php';
+
+
+?>
+
+<div id="wrapper"> 
+	<!-- end #header -->
+	<div id="page-bgtop"></div>
+	<div id="page">
+		<div><img src="images/pics01.jpg" width="920" height="300" alt="" /></div>
+		<div id="content">
+			<div class="post">
+				<h2 class="title"><a href="#">Welcome to Smart Academy </a></h2>
+				
+				<div style="clear: both;">&nbsp;</div>
+				<div class="entry">
+					content goes here.
+				</div>
+			</div>
+			
+			<div style="clear: both;">&nbsp;</div>
+		</div>
+		<!-- end #content -->
+		<div id="sidebar">
+			<ul>
+				
+				
+				<li>
+					<h2>Activity</h2>
+					<ul>
+						<li><a href="#">UCMAS</a></li>
+						<li><a href="#">Dance</a></li>
+						<li><a href="#">Drawing</a></li>
+						<li><a href="#">Art</a></li>
+						<li><a href="#">Canvas Painting</a></li>
+						<li><a href="#">Skting</a></li>
+					</ul>
+				</li>
+				
+			</ul>
+		</div>
+		<!-- end #sidebar -->
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<div id="page-bgbtm"></div>
+	<!-- end #page --> 
+</div>
+<div id="footer">
+	<p>2021. Website Manage By Tanvi Patel</p>
+</div>
+<!-- end #footer -->
+</body>
+</html>
